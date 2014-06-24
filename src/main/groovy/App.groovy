c974b00fe9d3d42010909abf0fd0e799eff4da5c
int128 <int128@gmail.com>
2014-06-24 15:16:38 +0900

    Add the example controller

--- src/main/groovy/App.groovy
+++ src/main/groovy/App.groovy
@@ -1,10 +1,11 @@
-import org.springframework.boot.*
-import org.springframework.boot.autoconfigure.*
-import org.springframework.stereotype.*
-import org.springframework.web.bind.annotation.*
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
-@RestController
+@Configuration
 @EnableAutoConfiguration
+@ComponentScan
 @groovy.transform.CompileStatic
 class App {
   static void main(String[] args) {
